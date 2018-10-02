2d2cd205dc69bbedd51e929579ba5e70b788197e
Takafumi Arakaki <aka.tkf@gmail.com>
2018-10-01 23:08:08 -0700

    Setup Documentation job

--- docs/deploy.jl
+++ docs/deploy.jl
@@ -1,8 +1,5 @@
 using Documenter
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/tkf/PyBase.jl.git",
+)
--- docs/make.jl
+++ docs/make.jl
@@ -1,2 +1,14 @@
+#!/bin/bash
+# -*- mode: julia -*-
+#=
+JULIA="${JULIA:-julia --color=yes --startup-file=no}"
+export JULIA_PROJECT="$(dirname ${BASH_SOURCE[0]})"
+
+set -ex
+${JULIA} -e 'using Pkg; Pkg.instantiate();
+             Pkg.develop(PackageSpec(path=pwd()))'
+exec ${JULIA} "${BASH_SOURCE[0]}" "$@"
+=#
+
 include("build.jl")
 include("deploy.jl")
